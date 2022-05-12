e8fcb47407e8288bebfb02972054a94cc70fdce1
aurnik <aurnikislam@gmail.com>
2022-05-11 17:34:06 -0700

    Base commit

--- README.md
+++ README.md
@@ -1,70 +1,27 @@
-# Getting Started with Create React App
+# How to run the app
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+1. `cd` into the project directory and run `npm install` to set up the dependencies
+2. Run the project with `npm start`
+3. The project should be viewable on [http://localhost:3000](http://localhost:3000)
 
-## Available Scripts
+# Questions
 
-In the project directory, you can run:
+1. How long did it take you to complete this assignment?
 
-### `npm start`
+_About 3-4 hours start to finish._
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+2. What about this assignment did you find most challenging?
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+_It was interesting tracking user interaction and translating that within the confines of the canvas._
 
-### `npm test`
+3. What about this assignment did you find unclear?
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+_Nothing, I thought it was straightforward._
 
-### `npm run build`
+4. Do you feel like this assignment has an appropriate level of difficulty?
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+_I think it's a fair assessment considering the type of work that would be required for Streamlabs Desktop._
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+5. Briefly explain the technical decisions you made in this project, i.e. architecture, code-splitting, libraries, or other decisions and tradeoffs.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+_Originally I was thinking of just going the simple route because this project could really be done with just plain HTML and JavaScript. However, based on the functionality needed for the "future features", I decided to build it out with React and npm. Again, it was a choice that made sense considering the stack used for Streamlabs Desktop._
--- src/App.js
+++ src/App.js
@@ -1,23 +1,194 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useEffect, useRef, useState } from "react";
+
+let ctx; // context for the canvas
 
 function App() {
+  const canvasRef = useRef();
+  const [images, setImages] = useState([]); // this will hold all of the images
+  const [activeElements, setActiveElements] = useState([]); // this will hold everything being dragged
+
+  const loadImage = (src) => {
+    // Initial check to see that image isn't already loaded
+    if (!images.some((img) => img.src === src)) {
+      const image = new Image();
+      image.src = src;
+      image.onload = () => {
+        const { width, height } = image;
+        setImages((prevImages) => {
+          // If image is already in the array, don't add it again
+          if (prevImages.some((img) => img.src === src)) return prevImages;
+
+          // Randomize position of image, constraining it to the canvas area
+          const x = Math.random() * (canvasRef.current.width - width);
+          const y = Math.random() * (canvasRef.current.height - height);
+
+          return [
+            ...prevImages,
+            {
+              image,
+              src,
+              width,
+              height,
+              x,
+              y,
+
+              // Allows image to scale with the canvas
+              widthRatio: width / canvasRef.current.width,
+              heightRatio: height / canvasRef.current.height,
+
+              // Used for keeping track of the position from which the image is being dragged
+              xLast: x,
+              yLast: y,
+
+              // Allows image position to scale with the canvas
+              xRatio: x / canvasRef.current.width,
+              yRatio: y / canvasRef.current.height,
+            },
+          ];
+        });
+      };
+    }
+  };
+
+  // Given a mousedown event and an image src, save the position of the mouse and activte the image for dragging
+  const activateDrag = (e, src) => {
+    e.preventDefault();
+    setActiveElements((prevActiveElements) => {
+      if (prevActiveElements.some((el) => el.src === src)) {
+        return prevActiveElements;
+      }
+      return [...prevActiveElements, { src, x: e.clientX, y: e.clientY }];
+    });
+  };
+
+  // Given an image src, remove it from the active elements and update its position
+  const deactivateDrag = (src) => {
+    setImages((prevImages) =>
+      prevImages.map((img) => {
+        const activeDrag = activeElements.find((el) => el.src === img.src);
+        if (activeDrag) {
+          // Update the additional position properties to reflect the new position of the image
+          img.xLast = img.x;
+          img.yLast = img.y;
+          img.xRatio = img.x / canvasRef.current.width;
+          img.yRatio = img.y / canvasRef.current.height;
+        }
+        return img;
+      })
+    );
+    setActiveElements((prevActiveElements) => {
+      return prevActiveElements.filter((el) => el.src !== src);
+    });
+  };
+
+  const dragImage = (e) => {
+    e.preventDefault();
+
+    // Check that there are any active elements to begin with
+    if (activeElements.length === 0) return;
+
+    setImages((prevImages) =>
+      prevImages.map((img) => {
+        const activeDrag = activeElements.find((el) => el.src === img.src);
+        if (activeDrag) {
+          const movedX = img.xLast + (e.clientX - activeDrag.x);
+          const movedY = img.yLast + (e.clientY - activeDrag.y);
+
+          // Constrain the movement of the image to fit within the canvas
+          const leftConstraint = 0;
+          const rightConstraint = canvasRef.current.width - img.width;
+          const topConstraint = 0;
+          const bottomConstraint = canvasRef.current.height - img.height;
+
+          if (movedX < leftConstraint) img.x = leftConstraint;
+          else if (movedX > rightConstraint) img.x = rightConstraint;
+          else img.x = movedX;
+
+          if (movedY < topConstraint) img.y = topConstraint;
+          else if (movedY > bottomConstraint) img.y = bottomConstraint;
+          else img.y = movedY;
+        }
+        return img;
+      })
+    );
+  };
+
+  // Scales the canvas and images to follow the canvas element's responsive resizing
+  const handleResize = () => {
+    if (ctx && canvasRef.current) {
+      // These are the updated dimensions of the canvas element
+      const updatedWidth = canvasRef.current.offsetWidth;
+      const updatedHeight = canvasRef.current.offsetHeight;
+
+      // Resize the dimensions of the canvas
+      ctx.canvas.width = updatedWidth;
+      ctx.canvas.height = updatedHeight;
+
+      // Resize the images and proportionally update the positions of the images
+      setImages((prevImages) => {
+        return prevImages.map((img) => {
+          img.width = img.widthRatio * updatedWidth;
+          img.height = img.heightRatio * updatedHeight;
+          img.x = img.xRatio * updatedWidth;
+          img.y = img.yRatio * updatedHeight;
+          img.xLast = img.x;
+          img.yLast = img.y;
+          return img;
+        });
+      });
+    }
+  };
+
+  // When component is first loaded
+  useEffect(() => {
+    // Call the resize function once to set the initial dimensions of the canvas
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Load images
+    loadImage("/cat.png");
+    loadImage("/dog.png");
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  // When the canvas is first loaded
+  useEffect(() => {
+    ctx = canvasRef.current.getContext("2d");
+  }, [canvasRef.current]);
+
+  // Redraw images whenever there is a change in the images array
+  useEffect(() => {
+    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    images.forEach(({ image, width, height, x, y }) => {
+      ctx.drawImage(image, x, y, width, height);
+    });
+  }, [images]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="canvasWrapper">
+        <canvas ref={canvasRef}></canvas>
+        <div className="draggablesWrapper">
+          {images.map(({ src, width, height, x, y }) => (
+            <div
+              className={`draggable ${
+                activeElements.some((el) => el.src === src) ? "active" : ""
+              }`}
+              key={src}
+              style={{ width, height, left: x, top: y }}
+              onMouseDown={(e) => activateDrag(e, src)}
+              onMouseUp={() => deactivateDrag(src)}
+              onMouseOut={() => deactivateDrag(src)}
+              onMouseMove={dragImage}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
